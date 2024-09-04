cf89ad9f617d879cb14a53faa2ac771811f9ff30
FelipeC-05 <barbosacarvalhodoamaralfelipe@gmail.com>
2024-09-04 20:03:15 -0300

    Finalizando projeto

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,21 +1,23 @@
-function adicionar(produto){
+function alterarQtd(produto, acao){
     const qtd = document.getElementById('qtd_' + produto);
     const valor = document.getElementById('valor_' + produto);
     const total = document.getElementById('total_' + produto);
     
-    qtd.innerHTML = Number(qtd.innerHTML) + 1;
+    if (acao == '-' && qtd.innerHTML == 0){
+        alert('Atençao, a quantidade nao pode ser menor que zero')
+    }
+    else{
+    acao == '+'? qtd.innerHTML++ : qtd.innerHTML--
     total.innerHTML = qtd.innerHTML * valor.innerHTML;
-
-    console.log(qtd.innerHTML) + 1;
+    soma()
+    }
+    
 }
 
-function retirar(produto){
-    const qtd = document.getElementById('qtd_' + produto);
-    const valor = document.getElementById('valor_' + produto);
-    const total = document.getElementById('total_' + produto);
-    
-    qtd.innerHTML = Number(qtd.innerHTML) - 1;
-    total.innerHTML = qtd.innerHTML * valor.innerHTML;
-
-    console.log(qtd.innerHTML) - 1;
+function soma(){
+    let soma = 0
+    for(let i = 1; i < 4; i++){
+    soma += Number(document.getElementById('total_' + i).innerHTML)
+}
+    document.getElementById('subtotal').innerHTML = soma;
 }
